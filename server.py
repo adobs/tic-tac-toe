225bec8ca9c240c3314330ef92a988ca36c143c5
adobs <dobs@u.northwestern.edu>
2016-01-11 17:50:37 -0800

    first commit

---
+++ server.py
@@ -0,0 +1,37 @@
+"""
+server.py
+
+File contains all routes for Flask app
+"""
+
+from flask import Flask, request, render_template, jsonify
+from jinja2 import StrictUndefined
+import os
+
+app = Flask(__name__)
+
+app.secret_key = os.environ.get("FLASK_SECRET_KEY", "ABCDEF")
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route("/")
+def home():
+    """ Home page """
+
+    return render_template("index.html")
+
+
+@app.route("/scrap")
+def scrap():
+    """ Home page """
+
+    return render_template("scrap.html")
+
+
+
+if __name__ == "__main__":
+    
+    PORT = int(os.environ.get("PORT", 5000))
+    DEBUG = "NO_DEBUG" not in os.environ
+
+    app.run(debug=DEBUG, host="0.0.0.0", port=PORT)
