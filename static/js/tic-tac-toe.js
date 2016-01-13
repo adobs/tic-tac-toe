0f56327284d2df522e13fe6a4fc88ee18e6b7c6c
Alexandra Dobkin <Alexandra Dobkin>
2016-01-13 07:31:11 -0800

    fixed responsiveness, testing passed

--- static/js/tic-tac-toe.js
+++ static/js/tic-tac-toe.js
@@ -33,20 +33,20 @@ $(function(){
     // create board based on form input
     function createBoard(evt){
 
-
         // empty board
         $("#board").empty();
         evt.preventDefault();
-        n = parseInt($("input#num").val());
+        var n = parseInt($("input#num").val());
 
         // figure out dimenions based on window size
-        var btnDimension = (($("#page-div").width() - n*2)/n);
-        $("#board").css("max-width", (btnDimension*n));
+        var btnDimension = Math.min(($(window).width() - 100)/n, ($(window).height()-100)/n);
+        // $("#board").css("max-width", (btnDimension*n));
 
         // loop through (2x) to create rows and columns of board as buttons
         for (var i=0; i<n; i++){
+            $("#board").append("<div class='row' id=board-row-"+i+"></div>")
             for(var j=0; j<n; j++){
-                $("#board").append("<button style='height:"+btnDimension+";width:"+btnDimension+";padding:0;margin:0' class='btn btn-default board-btn' data-row='"+i+"' data-column='"+j+"'}></button>");
+                $("#board-row-"+i).append("<button style='height:"+btnDimension+";width:"+btnDimension+";padding:0;margin:0' class='btn btn-default board-btn' data-row='"+i+"' data-column='"+j+"'}></button>");
             }
         }
 
@@ -58,12 +58,12 @@ $(function(){
     function resetBoard(evt){
         evt.preventDefault();
 
-        n = parseInt($("input#num").val());
+        var n = parseInt($("input#num").val());
 
-        // iterate through tiles on board and empty/enable them
+        // iterate through buttons on board and empty/enable them
         for (var m=0; m<(n*n); m++){
-            $("#board")[0].children[m].disabled=false;
-            $("#board")[0].children[m].innerHTML="";
+            $(".board-btn")[m].disabled=false;
+            $(".board-btn")[m].innerHTML="";
         }
 
     }
@@ -95,21 +95,21 @@ $(function(){
     }
     
     function addComputerMove(move){
-        n = parseInt($("input#num").val());
+        var n = parseInt($("input#num").val());
         
         // the open array will collect the indices of open spots
         var open = [];
 
-        for (var counter =0; counter<(n*n-1); counter++){
-            if (($("#board")[0].children[counter].innerHTML)===""){
+        for (var counter =0; counter<(n*n); counter++){
+            if (($(".board-btn")[counter].innerHTML)===""){
                 open.push(counter);
             }
         }
 
         // randomly select an open index for the computer to play
         var index = _.sample(open, 1);
-        $("#board")[0].children[index].innerHTML = move;
-        $("#board")[0].children[index].disabled = true;
+        $(".board-btn")[index[0]].innerHTML = move;
+        $(".board-btn")[index[0]].disabled = true;
         
         checkBoard(move);
 
@@ -128,7 +128,7 @@ $(function(){
 
     function checkBoard(move){
 
-        n = parseInt($("input#num").val());
+        var n = parseInt($("input#num").val());
 
         var winner;
         
@@ -136,7 +136,7 @@ $(function(){
         var countBoard = 0;
 
         for (var r=0; r< n*n; r++){
-            if(($("#board")[0].children[r].innerHTML)!==""){
+            if(($(".board-btn")[r].innerHTML)!==""){
                 countBoard += 1;
             }
         }
@@ -151,7 +151,7 @@ $(function(){
             count1 = 0;
             // ... then iterates through the next item in the column (+=n)
             for (var k=a; k<(n*n); k+=n){
-                if (($("#board")[0].children[k].innerHTML)===move){
+                if (($(".board-btn")[k].innerHTML)===move){
                     count1 += 1;
                 }
                 if (count1===n){
@@ -167,7 +167,7 @@ $(function(){
             count2 = 0;
             // ... then iterates through the next item in the row (+=1)
             for (var l=b; l<(b+n); l+=1){
-                if (($("#board")[0].children[l].innerHTML)===move){
+                if (($(".board-btn")[l].innerHTML)===move){
                     count2 += 1;
                 }
                 if (count2===n){
@@ -180,7 +180,7 @@ $(function(){
         var count3 = 0;
         // single for loop iterates across the board
         for (var c=0; c<(n*n); c+=(n+1)){
-            if (($("#board")[0].children[c].innerHTML)===move){
+            if (($(".board-btn")[c].innerHTML)===move){
                 count3 += 1;
             }
             if (count3===n){
@@ -192,7 +192,7 @@ $(function(){
         var count4 = 0;
         // single for loop iterates across the board
         for (var d=n-1; d<=(n*n-n); d+=(n-1)){
-            if (($("#board")[0].children[d].innerHTML)===move){
+            if (($(".board-btn")[d].innerHTML)===move){
                 count4 += 1;
             }
             if (count4===n){
@@ -206,7 +206,7 @@ $(function(){
 
             // disable the board
             for (var p=0; p<(n*n); p++){
-                $("#board")[0].children[p].disabled=true;
+                $(".board-btn")[p].disabled=true;
             }
             return true;
 
@@ -233,15 +233,14 @@ $(function(){
 });
 
 function resizeApp(){
-    n = parseInt($("input#num").val());
+    var n = parseInt($("input#num").val());
 
     // make buttons change size
-    var btnDimension1 = (($("#page-div").width() - n*2)/n);
+    var btnDimension1 = Math.min(($(window).width() - 100)/n, ($(window).height()-100)/n);
     $(".board-btn").width(btnDimension1);
     $(".board-btn").height(btnDimension1);
-    $("#board").width(btnDimension1*n +n*2);
+    // $("#board").width(btnDimension1*n +n*2);
     $(".board-btn").css("font-size",btnDimension1).css("line-height", ".75em");
-
 
     // make background-div different height
     var row1Height1 = $("#row1").height();
@@ -249,4 +248,4 @@ function resizeApp(){
     $("#background-div").height(20+row1Height1+row2Height1);
 }
 // event listener for window resizing
-  $(window).resize(resizeApp);
+$(window).resize(resizeApp);
